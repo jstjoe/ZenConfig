aec5a8b1b2aeb653ed204806df1c8d1a85ea86a8
Joseph <joe@josephmccarron.com>
2014-01-26 09:36:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZenConfig::Application.config.secret_key_base = 'eee0ba95df7b62429aad5be33aac0ca09ee747e4b20fc330b765b2faba2126b7996e6a0fbd6c8705e12572f75d3f36e7ac70501e456be3a663f69c319380c7ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZenConfig::Application.config.session_store :cookie_store, key: '_ZenConfig_session'
